c14c711586ace31b6938b7ec8a4aee5bb63ffb1c
whyinzoo@gmail.com <whyinzoo@gmail.com>
2014-03-07 21:43:58 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProveIt::Application.config.secret_token = '10617a8bde2c685b86ae64321c0f48f6520c0bb468bddac29ed3e79833635f490d52abb898e4bb6514602af91d2515d204ef8f87ceab74675d35f821b637acd4'
